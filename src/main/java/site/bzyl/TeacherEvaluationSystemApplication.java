939a53fe97a0f495613f7837653692b2c8c35066
Cymbospondylus <2810560593@qq.com>
2024-10-08 23:26:40 +0800

    init:初始化项目，添加readme

---
+++ src/main/java/site/bzyl/TeacherEvaluationSystemApplication.java
@@ -0,0 +1,11 @@
+package site.bzyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherEvaluationSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherEvaluationSystemApplication.class, args);
+    }
+}
